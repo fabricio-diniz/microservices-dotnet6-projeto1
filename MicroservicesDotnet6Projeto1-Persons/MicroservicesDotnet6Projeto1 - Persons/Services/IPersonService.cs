672715bdff02db028b795429d20aeb975de65444
Fabrício Diniz <fabricio_diniz@outlook.com>
2022-06-25 20:29:17 -0300

    Add 2 news solutions;

---
+++ MicroservicesDotnet6Projeto1-Persons/MicroservicesDotnet6Projeto1 - Persons/Services/IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using MicroservicesDotnet6Projeto1.Model;
+
+namespace MicroservicesDotnet6Projeto1.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person Update(Person person);
+        Person FindById (long id);
+        List<Person> FindAll();
+        void Delete(long id);
+    }
+}
